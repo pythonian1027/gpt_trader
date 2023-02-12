9d50b52113a16955080291018c958f5d56dfddc0
Ricardo Cortez <ricardocortez21@gmail.com>
2023-02-12 18:23:31 -0500

    Initial commit

--- hello_flask.py
+++ hello_flask.py
@@ -1,7 +1,24 @@
-from flask import Flask
+# from flask import Flask
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello_flask():
+#     return 'Hello, World!'
+
+from flask import Flask, jsonify, Response
+import gdown
+import subprocess
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
+def hello_flask():    
+    url = 'https://drive.google.com/file/d/1ODbfWm7SaV-r5r9clcUdQdmcDBx0NJSy/view?usp=sharing'
+    output="colab.ipynb"
+    gdown.download(url, output,  fuzzy = True, quiet=False)
+    
+    # execute the results
+    print ('running colab notebook')
+    result = subprocess.run(["jupyter", "nbconvert", "--execute", "colab.ipynb"], capture_output=True, text=True)
+    print (result.stdout)
+    # return jsonify(message='colab notebook ran successfully')
+    return Response(result.stdout, content_type='text/plain')
