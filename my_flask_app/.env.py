e97892d2d9493cb4805138afbfe630e902da98dd
Ricardo Cortez <ricardocortez21@gmail.com>
2023-02-12 19:51:27 -0500

    added flask app folder

---
+++ my_flask_app/.env.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 12 19:49:20 2023
+
+@author: ricor
+"""
+
+
+# .env (optional)
+FLASK_APP=run.py
+FLASK_ENV=development
---
+++ my_flask_app/run.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 12 19:49:50 2023
+
+@author: ricor
+"""
+
+
+# run.py (optional)
+from app import app
+
+if __name__ == '__main__':
+    app.run()
